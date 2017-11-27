97709a90ac0a6fd7cb308d95368775b9d032053f
bryan <bryan.ransil@gmail.com>
2017-11-26 23:29:18 -0800

    Access post tags of all photos
    We need to look at the tags to know which categories the photos are sorted
    into.

---
+++ extractBlog.py
@@ -0,0 +1,12 @@
+import pytumblr
+
+# Authenticate via API Key
+client = pytumblr.TumblrRestClient('5tpUWunGLQwglO70dKIURJ6FkYTfs4aPSyFKb01Q4qD4M4LMks')
+blog = 'artcive'
+
+# Make the request
+postResponse = client.posts(blog + '.tumblr.com', type='photo')
+if postResponse.get('posts', []) != []:
+    postList = postResponse.get('posts', [])
+    for post in postList :
+        print post.get('tags')
