19792b4dd0586552e68b76a6d38c5743642a7341
bryan <bryan.ransil@gmail.com>
2017-11-27 00:02:44 -0800

    Count all legal posts in blog
    We want to iterate over all posts in the blog, so we make a small sample
    that counts all legal posts in the blog

--- extractBlog.py
+++ extractBlog.py
@@ -2,12 +2,19 @@ import pytumblr
 
 # Authenticate via API Key
 client = pytumblr.TumblrRestClient('5tpUWunGLQwglO70dKIURJ6FkYTfs4aPSyFKb01Q4qD4M4LMks')
-blog = 'artcive'
+blogName = 'artcive'
+blog = blogName + '.tumblr.com'
 
 # Make the request
-postResponse = client.posts(blog + '.tumblr.com', type='photo')
-if postResponse.get('posts', []) != []:
-    postList = postResponse.get('posts', [])
-    for post in postList :
-        if len(post.get('photos', [])) <= 1 and 'pose' in post.get('tags') :
-            print "Here!"
+numPosts = client.blog_info(blog).get('blog').get('posts', 0)
+numCollectedPosts = 0
+print numPosts
+for offset in range(0, numPosts, 20) :
+    postResponse = client.posts(blog, type='photo', offset=offset)
+    if postResponse.get('posts', []) != []:
+        postList = postResponse.get('posts', [])
+        for post in postList :
+            if len(post.get('photos', [])) <= 1 and 'pose' in post.get('tags') :
+                numCollectedPosts += 1
+
+print numCollectedPosts
