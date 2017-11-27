0fd808b8f4e7a77e112714fe62728fee61c8ed35
bryan <bryan.ransil@gmail.com>
2017-11-27 01:21:57 -0800

    Download images from the blog
    Once we deal with all of the ways the name can be defined (including how
    some posts use utf8 characters), we can launch this script to download all
    of the images in the correct tag.
    
    At this point, we can scrape all images that are in a particular category
    and use it for model retraining.

--- extractBlog.py
+++ extractBlog.py
@@ -1,15 +1,46 @@
+import imghdr
+import mimetypes
+import os
 import pytumblr
+import requests
+import sys
 import urllib
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+class PhotoNameGenerator(object):
+    def __init__(self):
+        self.defaultImageNumber = -1
+        self.defaultName = 'unnamedImage'
+
+    def __call__(self, post, url):
+        response = requests.get(url)
+        content_type = response.headers['content-type']
+        extension = mimetypes.guess_extension(content_type)
+        name = post.get('slug', self.defaultName)
+        print extension
+        if (name == self.defaultName or name == '') :
+            self.defaultImageNumber += 1
+            return self.defaultName + str(self.defaultImageNumber) + extension
+        else :
+            return name + extension
+    
 
 # Authenticate via API Key
 client = pytumblr.TumblrRestClient('5tpUWunGLQwglO70dKIURJ6FkYTfs4aPSyFKb01Q4qD4M4LMks')
+
+# Set up global variables
 blogName = 'artcive'
 blog = blogName + '.tumblr.com'
+testfile = urllib.URLopener()
+photoNamer = PhotoNameGenerator()
+directory = "images/"
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
-# Make the request
 numPosts = client.blog_info(blog).get('blog').get('posts', 0)
-numCollectedPosts = 0
-print numPosts
+
 for offset in range(0, numPosts, 20) :
     postResponse = client.posts(blog, type='photo', offset=offset)
     if postResponse.get('posts', []) != []:
@@ -17,8 +48,7 @@ for offset in range(0, numPosts, 20) :
         for post in postList :
             if len(post.get('photos', [])) == 1 and 'pose' in post.get('tags') :
                 photoEntry = post.get('photos', [])[0].get('original_size')
-                testfile = urllib.URLopener()
-                testfile.retrieve(photoEntry.get('url'), post.get('slug', 'default'))
-
-
-print numCollectedPosts
+                photoName = photoNamer(post, photoEntry.get('url'))
+                print photoName
+                fullfilename = os.path.join(directory, photoName)
+                testfile.retrieve(photoEntry.get('url'), fullfilename)
